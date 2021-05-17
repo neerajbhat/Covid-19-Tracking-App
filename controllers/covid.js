f00020007b8cb57fae30baa125a19e83db5c9a66
neerajbhat <er.neeraj0007@gmail.com>
2021-05-17 13:13:37 +0530

    Initial Commit

--- controllers/covid.js
+++ controllers/covid.js
@@ -1,17 +1,10 @@
 const fetch = require("node-fetch");    
 exports.getCovidData = async (req, res, next) => {
     try {
-        const request = require('request');
-        // request('https://api.covid19india.org/data.json', { json: true }, (err, res, covidData) => {
-        //     if (err) { return console.log(err); }
-        //     console.log(covidData.statewise[0]);
-        // });
-        // console.log(JSON.stringify(xxx));
-        // return xxx;
+        // const request = require('request');
+
         const response = await fetch('https://api.covid19india.org/data.json');
         const covidData = await response.json();
-        // console.log(covidData.statewise[0]);
-        // return covidData.statewise[0].active;
 
         var covidReport = [];
         for (let i = 0; i < covidData.statewise.length; i++) {
--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 const covid = require("../controllers/covid");
-// router.get("/", async (req, res, next) => {
-//     const covidData = covid.getCovidData();
-//     console.log("hi = "+covidData);
-//     res.render("index", {covidData: covidData});
-// });
 
 router.get("/", covid.getCovidData);
-
-// routers will be here
-// router.get("/", (req, res) => {
-//     const covid = require("../controllers/covid");
-//     console.log(covid.getCovidData);
-//     res.render("index", {covidData: covid});
-// });
 
 module.exports = router;
